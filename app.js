ff717173282f350869164c85f4d26513128e51b8
Conny Brunnkvist <cbrunnkvist@gmail.com>
2014-02-16 16:20:39 +0700

    Renaming files

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ var server = http.createServer();
 
 var port = Number(process.env.PORT || 5000);
 
-var isitholidayResponseHandler = require('./lib/response-handler.js');
+var isitholidayResponseController = require('./lib/web/response-controller.js');
 
 server.listen(port, function() {
   console.log('Listening on %d', port);
   server.on('request', function(request, response) {
-    response.write(isitholidayResponseHandler(request.url));
+    response.write(isitholidayResponseController(request.url));
     response.end('\n');
   });
 });
--- test/isitholidayResponseHandler_test.js
+++ test/isitholidayResponseHandler_test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var isitholidayResponseHandler = require('../lib/response-handler.js');
+var isitholidayResponseController = require('../lib/web/response-controller.js');
 
 exports['override'] = {
   setUp: function(done) {
@@ -8,12 +8,12 @@ exports['override'] = {
     done();
   },
   'with yes': function(test) {
-    this.responseBody = isitholidayResponseHandler('http://localhost/?answer=yes');
+    this.responseBody = isitholidayResponseController('http://localhost/?answer=yes');
     test.ok(this.responseBody.match(/\? y/i), 'should show y(es)');
     test.done();
   },
   'with no': function(test) {
-    this.responseBody = isitholidayResponseHandler('http://localhost/?answer=no');
+    this.responseBody = isitholidayResponseController('http://localhost/?answer=no');
     test.ok(this.responseBody.match(/\? n/i), 'should show n(o)');
     test.done();
   },
