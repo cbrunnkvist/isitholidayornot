43d9f45efb8aa40fc337b5b285042db1790a406d
Conny Brunnkvist <cbrunnkvist@gmail.com>
2014-02-16 15:26:15 +0700

    Rename & refactor response handler

--- TODO.md
+++ TODO.md
@@ -11,7 +11,7 @@ HolidayDb lookup
 
 Web interface
 -------------
-* Implement the most basic http server frontend with ?answer=(yes|no) override
+* [DONE] Implement the most basic http server frontend with ?answer=(yes|no) override
 * Ask Bower to pull in HTML components e.g. Bootstrap
 
 Deployment
--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var server = http.createServer();
 
 var port = Number(process.env.PORT || 5000);
 
-var isitholidayHttpHandler = require('./lib/http-handler.js');
+var isitholidayResponseHandler = require('./lib/response-handler.js');
 
 server.listen(port, function() {
 	console.log('Listening on %d', port);
-	server.on('request', isitholidayHttpHandler);
+	server.on('request', function(request, response) {
+		response.write(isitholidayResponseHandler(request.url));
+		response.end('\n');
+	});
 });
