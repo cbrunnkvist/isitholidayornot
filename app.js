b46c2c3d6aad87861fa370bcb27f2c8a17f17322
Conny Brunnkvist <cbrunnkvist@gmail.com>
2014-02-16 14:30:41 +0700

    Add basic http handler

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+var http = require('http');
+var server = http.createServer();
+
+var port = Number(process.env.PORT || 5000);
+
+var isitholidayHttpHandler = require('./lib/http-handler.js');
+
+server.listen(port, function() {
+	console.log('Listening on %d', port);
+	server.on('request', isitholidayHttpHandler);
+});
